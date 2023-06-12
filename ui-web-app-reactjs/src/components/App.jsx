22882f9ea394cad49200fcbc3d07699b0d41e3a7
HemanthGowda01 <tinycamvlogs@gmail.com>
2023-06-12 15:31:04 +0000

    Your commit message new

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [displayData, setDisplayData] = useState({url:''})
 
   const handleApiCall = async (e) => {
-    const url = 'http://34.67.30.154:9999/'+e.target.name;
+    const url = 'http://54.236.81.27:9999/'+e.target.name;
     displayData.url=url
     let options = {}
       try {
